d76535c2ca8763f0e6fcf2ab63e54ce59f2df0f4
张新 <zhangxin@zhangxindemacbook-pro.local>
2016-08-16 22:56:16 +0800

    Initial Commit

---
+++ RouterDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RouterDemo
+//
+//  Created by 张新 on 16/8/16.
+//  Copyright © 2016年 voidxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RouterDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RouterDemo
+//
+//  Created by 张新 on 16/8/16.
+//  Copyright © 2016年 voidxin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
